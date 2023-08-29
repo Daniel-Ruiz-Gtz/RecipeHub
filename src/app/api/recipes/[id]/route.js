e2c29470f26291129d14625c8d4dd19557024c6a
Daniel-Ruiz-Gtz <58959667+Daniel-Ruiz-Gtz@users.noreply.github.com>
2023-08-28 21:34:35 -0600

    GET Recipe

--- src/app/api/recipes/[id]/route.js
+++ src/app/api/recipes/[id]/route.js
@@ -1,7 +1,32 @@
 import { NextResponse } from "next/server";
+import { conn } from "@/libs/mysql";
+import { metadata } from "@/app/layout";
 
-export function GET() {
-  return NextResponse.json("Obteniendo un Producto");
+export async function GET(request, { params }) {
+  try {
+    const result = await conn.query("SELECT * FROM recipe WHERE id = ?", [
+      params.id,
+    ]);
+    if (result.length == 0) {
+      return NextResponse.json(
+        {
+          message: "Receta no Encontrada",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse(
+      {
+        message: error.message,
+      },
+      { status: 500 }
+    );
+  }
 }
 
 export function DELETE() {
